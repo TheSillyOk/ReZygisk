3e0569b766f255c735ff9233594abb069e48a052
TheSillyOk <194730369+TheSillyOk@users.noreply.github.com>
2026-09-13 00:02:59 -0300

    improve: use Termux's sysroot The standard Termux sysroot package doesn't require a sysroot path to be specified, so we only force it to be set when a build is ran outside Termux and don't set the sysroot argument when $(SYSROOT) isn't specified.

--- common.mk
+++ common.mk
@@ -20,12 +20,12 @@ NDK_VERSION ?= 29.0.13113456
 ANDROID_HOME ?= $(HOME)/Android/Sdk
 NDK_PATH ?= $(ANDROID_HOME)/ndk/$(NDK_VERSION)
 TOOLCHAIN = $(NDK_PATH)/toolchains/llvm/prebuilt/linux-x86_64
-SYSROOT = $(TOOLCHAIN)/sysroot
 
 ifeq ($(TERMUX_VERSION),)
 	CC = $(TOOLCHAIN)/bin/clang
 	AR = $(TOOLCHAIN)/bin/llvm-ar
 	STRIP = $(TOOLCHAIN)/bin/llvm-strip
+	SYSROOT = $(TOOLCHAIN)/sysroot
 else
 	CC = clang
 	AR = llvm-ar
@@ -39,7 +39,11 @@ TARGET_armeabi-v7a = armv7a-linux-androideabi$(API_LEVEL)
 TARGET_x86 = i686-linux-android$(API_LEVEL)
 TARGET_x86_64 = x86_64-linux-android$(API_LEVEL)
 
-CC_ARCH = $(CC) --target=$(TARGET_$(ARCH)) --sysroot=$(SYSROOT)
+ifneq ($(SYSROOT),)
+	CC_ARCH = $(CC) --target=$(TARGET_$(ARCH)) --sysroot=$(SYSROOT)
+else
+	CC_ARCH = $(CC) --target=$(TARGET_$(ARCH))
+endif
 
 NDK_CFLAGS = -DANDROID -fdata-sections -ffunction-sections -funwind-tables \
 	-fstack-protector-strong -no-canonical-prefixes -D_FORTIFY_SOURCE=2 \
